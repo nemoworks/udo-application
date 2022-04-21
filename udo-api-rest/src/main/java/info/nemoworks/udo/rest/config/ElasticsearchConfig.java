5be0495cc54647573029f12bbea64f6113eb1988
TcPaulyue <1195882464@qq.com>
2022-04-21 15:11:11 +0800

    docker-compse

--- udo-api-rest/src/main/java/info/nemoworks/udo/rest/config/ElasticsearchConfig.java
+++ udo-api-rest/src/main/java/info/nemoworks/udo/rest/config/ElasticsearchConfig.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -12,12 +13,15 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages = { "info.nemoworks.udo.repository.elasticsearch" })
 public class ElasticsearchConfig {
 
+    @Value("${elasticsearch.host}")
+    private String elasticsearchHost;
+
     @Bean
     RestHighLevelClient client() {
         RestHighLevelClient client = new RestHighLevelClient(
                 RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")));
+                        new HttpHost(elasticsearchHost, 9200, "http"),
+                        new HttpHost(elasticsearchHost, 9201, "http")));
         return client;
     }
 
